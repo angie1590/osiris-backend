f9ff6dd4e3958d58511bce0ebd8e8cc8f66f2bfd
Andrea Peralta <andreacpb90@hotmail.com>
2023-07-10 18:12:37 -0500

    Unit test for waiting that the db is ready

--- osirisBackend/core/tests/test_commands.py
+++ osirisBackend/core/tests/test_commands.py
@@ -15,5 +15,19 @@ class CommandTest(SimpleTestCase):
     def test_wait_for_db_ready(self,patch_check):
         """Test waiting for db when db is available"""
         patch_check.return_value = True
+
         call_command('wait_for_db')
+
         patch_check.assert_called_once_with(database=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patch_check):
+        """Test waiting for db when getting OperationalError"""
+        patch_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patch_check.call_count, 6)
+
+        patch_check.assert_called_with(database=['default'])
