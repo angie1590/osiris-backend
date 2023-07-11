c03917b7da3d86c728f53e835b593f78941f0990
Andrea Peralta <andreacpb90@hotmail.com>
2023-07-10 22:05:19 -0500

    Adding test and functionality to create and validate users and superusers

--- osirisBackend/core/tests/test_model.py
+++ osirisBackend/core/tests/test_model.py
@@ -5,8 +5,11 @@ Test for models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-class ModelTest(TestCase):
+class ModelTests(TestCase):
+    """Test models."""
+
     def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
@@ -16,3 +19,30 @@ class ModelTest(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        sample_emails = [
+            ['test1@EXAMPLE.com','test1@example.com'],
+            ['Test2@Example.com','Test2@example.com'],
+            ['TEST3@EXAMPLE.COM','TEST3@example.com'],
+            ['test4@example.COM','test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email,'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user withouth an email raises a ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('','test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
