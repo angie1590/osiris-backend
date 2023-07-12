92ea8c4c9a2e950e8c77c5f9c178c7ac887b2cb7
Andrea Peralta Bravo <andrea.peraltabravo@thoughtworks.com>
2023-07-12 12:20:23 -0500

    Implement create user API

--- osirisBackend/user/tests/test_user_api.py
+++ osirisBackend/user/tests/test_user_api.py
@@ -46,18 +46,17 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-   def test_password_too_short(self):    
-        """Test an error is returned if password less than 5 chars"""
+   def test_password_too_short_error(self):
+        """Test an error is returned if password less than 5 chars."""
         payload = {
             'email': 'test@example.com',
-            'password': 'testpass123',
-            'name': 'Test Name'
-        } 
-        create_user(**payload)
+            'password': 'pw',
+            'name': 'Test name',
+        }
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
-        )
+        ).exists()
         self.assertFalse(user_exists)
